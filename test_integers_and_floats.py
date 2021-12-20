a775e7632e234c401e6d3e291a07abce346465fa
unknown <yuqiliu@163.com>
2021-12-20 12:13:56 +0800

    git@github.com:Liu110110/integers_floats_lab.git

--- test_integers_and_floats.py
+++ test_integers_and_floats.py
@@ -1,13 +1,12 @@
 from unittest import TestCase
 from integers_and_floats import integer_division, float_integer_multiplication, inputs_are_strings
 
-
 class Test(TestCase):
     def test_integer_division(self):
-        self.assertEqual(integer_division(), type(1.0))
+        self.assertEqual(integer_division(), type(2222/8.4))
 
     def test_float_integer_multiplication(self):
-        self.assertEqual(float_integer_multiplication(), type(1.0))
+        self.assertEqual(float_integer_multiplication(), type(334*8.6))
 
     def test_inputs_are_strings(self):
-        self.assertEqual(inputs_are_strings(), 7 / 2.2)
+        self.assertEqual(inputs_are_strings(), type(73 / 23))
